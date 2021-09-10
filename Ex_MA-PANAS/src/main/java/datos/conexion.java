0880cf7edc42016246b5eb834e1c4d7d88953830
Arturo Vallejo González <71914027+arvago@users.noreply.github.com>
2021-09-10 15:47:41 -0500

    datos

--- Ex_MA-PANAS/src/main/java/datos/conexion.java
+++ Ex_MA-PANAS/src/main/java/datos/conexion.java
@@ -5,10 +5,39 @@
  */
 package datos;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
  *
- * @author rocky
+ * @author Arturo Vallejo
  */
-public class conexion {
+public class Conexion {
+    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/pruebajdbc?useSSL=false&useTimezone=true&serverTimeZone=true&allowPublicKeyRetrieval=true";
+    private static final String JDBC_USER = "root";
+    private static final String JDBC_PASSWORD = "Admin123";
     
+    public static Connection getConnection() throws SQLException{
+        return DriverManager.getConnection(JDBC_URL, JDBC_USER, JDBC_PASSWORD);
+    }
+    
+    public static void close(ResultSet rs) throws SQLException{
+        rs.close();
+    }
+    
+    public static void close(Statement stmt) throws SQLException{
+        stmt.close();
+    }
+    
+    public static void close(PreparedStatement stmt) throws SQLException{
+        stmt.close();
+    }
+    
+    public static void close(Connection conn) throws SQLException{
+        conn.close();
+    }
 }
